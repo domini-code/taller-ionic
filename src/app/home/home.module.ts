b40137289f928f1ce61908fea73a005c8aa12505
Bezael <bezel@gmail.com>
2020-10-11 11:57:07 +0200

    Shared module and imports

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
-
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SharedModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, ]
 })
 export class HomePageModule {}
